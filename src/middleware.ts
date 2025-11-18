8d7877e1942bd202f8ae56ea29f8f69386ae6841
lucasananou <lucasananou@gmail.com>
2025-11-18 14:59:55 +0400

    Mise en ligne Vercel-6

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,12 @@ const isProtectedRoute = createRouteMatcher(["/app(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    await auth.protect();
+    const { userId } = await auth();
+    if (!userId) {
+      const signInUrl = new URL("/sign-in", req.url);
+      signInUrl.searchParams.set("redirect_url", req.url);
+      return Response.redirect(signInUrl);
+    }
   }
 });
 
