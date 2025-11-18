3572233f6fb19e97562eb97b733cb3c855b96a74
lucasananou <lucasananou@gmail.com>
2025-11-18 13:24:12 +0400

    Lancement de l'app-2

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default {
   out: "./drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL!,
   },
 } satisfies Config;
 
